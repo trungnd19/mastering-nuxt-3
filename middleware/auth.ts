76f6e87b6cb6d8d09a189b70c0f7dfc181acf172
trungnguyenduc0101 <trungnguyenduc0101@gmail.com>
2023-07-24 07:10:52 +0700

    feat: finish chapter 4

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,9 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    if(to.params.chapterSlug === '1-chapter-1') {
-        return;
-    }
-    return navigateTo('/login')
-})
+  const user = useSupabaseUser();
+
+  // Nếu user đã log in => xem được mọi videos trong khóa học
+  if (user.value || to.params.chapterSlug === "1-chapter-1") {
+    return;
+  }
+  return navigateTo(`/login?redirectTo=${to.path}`);
+});
--- pages/course.vue
+++ pages/course.vue
@@ -1,66 +1,68 @@
 <template>
-	<div>
-		<div class="mb-12 prose">
-		  <h1>
-			<span class="font-medium">
-			  <span class="font-bold">{{ title }}</span>
-			</span>
-		  </h1>
-		</div>
-	
-		<div class="flex flex-row justify-center flex-grow">
-		  <div
-			class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
-		  >
-			<h3>Chapters</h3>
-			<div
-						class="space-y-1 mb-4 flex flex-col"
-						v-for="chapter in chapters"
-						:key="chapter.slug"
-					>
-						<h4>{{ chapter.title }}</h4>
-						<NuxtLink
-							v-for="(lesson, index) in chapter.lessons"
-							:key="lesson.slug"
-							class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
-							:to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
-							:class="{
-								'text-blue-500': lesson.path === $route.fullPath,
-								'text-gray-600': lesson.path !== $route.fullPath,
-							}"
-						>
-							<span class="text-gray-500">{{ index + 1 }}.</span>
-							<span>{{ lesson.title }}</span>
-						</NuxtLink>
-					</div>
-		  </div>
-	
-		  <div class="prose p-12 bg-white rounded-md w-[65ch]">
-			<NuxtErrorBoundary>
-				<NuxtPage />
-				<template #error="{ error }">
-					<p>Oh no, something broke!
-						<code>{{ error }}</code>
-					</p>
-					<button class="text-white bg-gray-500" @click="resetError(error)">Reset</button>
-	
-	
-				</template>
-			</NuxtErrorBoundary>
-		  </div>
-		</div>
-	</div>
+  <div>
+    <div class="mb-4 flex justify-between items-center w-full">
+      <h1 class="text-3xl">
+        <span class="font-medium">
+          <span class="font-bold">{{ title }}</span>
+        </span>
+      </h1>
+      <UserCard />
+    </div>
+
+    <div class="flex flex-row justify-center flex-grow">
+      <div
+        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
+      >
+        <h3>Chapters</h3>
+        <div
+          class="space-y-1 mb-4 flex flex-col"
+          v-for="chapter in chapters"
+          :key="chapter.slug"
+        >
+          <h4>{{ chapter.title }}</h4>
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
+            :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+            :class="{
+              'text-blue-500': lesson.path === $route.fullPath,
+              'text-gray-600': lesson.path !== $route.fullPath
+            }"
+          >
+            <span class="text-gray-500">{{ index + 1 }}.</span>
+            <span>{{ lesson.title }}</span>
+          </NuxtLink>
+        </div>
+      </div>
+
+      <div class="prose p-12 bg-white rounded-md w-[65ch]">
+        <NuxtErrorBoundary>
+          <NuxtPage />
+          <template #error="{ error }">
+            <p>
+              Oh no, something broke!
+              <code>{{ error }}</code>
+            </p>
+            <button class="text-white bg-gray-500" @click="resetError(error)">
+              Reset
+            </button>
+          </template>
+        </NuxtErrorBoundary>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
 const { title, chapters } = useCourse();
 async function resetError(error) {
-	// await navigateTo('/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3')
-	// error.value = null;
+  // await navigateTo('/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3')
+  // error.value = null;
 
-	// Use when nothing you can do but throw the error
-	throw createError({
-		fatal: true,
-		message: 'Fatal error'
-	})
+  // Use when nothing you can do but throw the error
+  throw createError({
+    fatal: true,
+    message: "Fatal error"
+  });
 }
 </script>
