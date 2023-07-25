47864922cbfb5f60cea00c0bbf3851d257329762
DESKTOP-98R8H4U\ADMIN <trungducng116@gmail.com>
2023-07-25 10:07:21 +0700

    feat: implement caching with useSessionStorage

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,14 +1,34 @@
+import { StorageSerializers } from "@vueuse/core";
+import { LessonWithPath } from "~~/types/course";
+
 export default async (chapterSlug: string, lessonSlug: string) => {
-  const { data, error } = await useFetch(
-    `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
+  const url = `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`;
+  const lesson = useSessionStorage<LessonWithPath>(url, null, {
+    // By passing null as default => not know which serializer to use
+    serializer: StorageSerializers.object,
+  });
+
+  if (!lesson.value) {
+    const { data, error } = await useFetch<LessonWithPath>(url, 
+    // {
+    //   pick: ['title', 'number']
+    //   lazy: true
+    // }
   );
 
-  if (error.value) {
-    throw createError({
-      ...error.value,
-      statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
-    });
+    if (error.value) {
+      throw createError({
+        ...error.value,
+        statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
+      });
+    }
+
+    lesson.value = data.value; // Gán lại data cho cache
+  } else {
+    console.log(
+      `Getting lesson ${lessonSlug} in chapter ${chapterSlug} from cache`
+    );
   }
 
-  return data;
+  return lesson;
 };
